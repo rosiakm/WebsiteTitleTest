3c08c8d868bd5393233463cc6f029707ae9d0f3a
matros <mateusz.rosiak89@gmail.com>
2022-08-26 12:05:50 +0200

    code format

--- src/test/java/exercise/configuration/DriverSetUp.java
+++ src/test/java/exercise/configuration/DriverSetUp.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DriverSetUp {
-    private static WebDriver driver;
+    private WebDriver driver;
 
     public WebDriver getDriver() {
         return driver;
