66bf7319f73cf248e156d12b3e7a314f8929cd97
matros <mateusz.rosiak89@gmail.com>
2022-09-08 09:59:26 +0200

    after adding logs

--- src/test/java/exercise/configuration/DriverSetUp.java
+++ src/test/java/exercise/configuration/DriverSetUp.java
@@ -6,27 +6,30 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DriverSetUp {
+    private static Logger log = LoggerFactory.getLogger(DriverSetUp.class);
     private WebDriver driver;
-
     public WebDriver getDriver() {
         return driver;
     }
-
     @BeforeAll
     public static void setupDriver() {
         WebDriverManager.chromedriver().setup();
+        log.info("WebDriver started successfully");
     }
-
     @BeforeEach
     public void setup() {
         driver = new ChromeDriver();
+        log.info("Chrome has been opened");
         driver.manage().window().maximize();
+        log.info("Browser has been maximized");
     }
-
     @AfterEach
     public void tearDown() {
         driver.quit();
+        log.info("WebDriver has been quit");
     }
 }
--- src/test/java/exercise/tests/WebsiteTitleTest.java
+++ src/test/java/exercise/tests/WebsiteTitleTest.java
@@ -5,20 +5,20 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class WebsiteTitleTest extends DriverSetUp {
-
-    @ParameterizedTest
+    private static Logger log = LoggerFactory.getLogger(WebsiteTitleTest.class);
+    @ParameterizedTest(name = "{0} has been verified")
     @CsvFileSource(resources = "/data.csv")
     @Tag("Regression")
     @DisplayName("Website title tests")
-    void websiteTitleTest(String websiteAddress, String expectedTitle) {
+    void websiteTitleTest(String title, String websiteAddress, String expectedTitle) {
         getDriver().get(websiteAddress);
-        String actualTitle = getDriver().getTitle();
-
-        assertThat(actualTitle).isEqualTo(expectedTitle);
+        log.info("My website address is: " + websiteAddress);
+        assertThat(getDriver().getTitle()).isEqualTo(expectedTitle);
     }
-
 }
